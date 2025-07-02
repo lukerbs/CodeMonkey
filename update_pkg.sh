629eec7b63a5ddcf54c0e62a081dcb2738fc55fb
Luke Kerbs <LDK.kerbs@gmail.com>
2025-07-01 23:46:01 -0700

    improved build script

--- update_pkg.sh
+++ update_pkg.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-pip3 uninstall scriptmonkey
+# Clean uninstall and remove all build artifacts
+pip3 uninstall scriptmonkey -y
 rm -rf dist/
+rm -rf build/
+rm -rf *.egg-info
+
+# Build and upload
 python3 setup.py sdist bdist_wheel
 twine upload dist/*
